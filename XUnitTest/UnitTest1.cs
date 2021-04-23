14069cec261cb39f4ca9c464bd3382188897f1f6
Eduardo Rocha <Devops_Eduardo@outlook.com>
2021-04-23 19:52:58 +0000

    Updated UnitTest1.cs

--- XUnitTest/UnitTest1.cs
+++ XUnitTest/UnitTest1.cs
@@ -10,10 +10,6 @@ namespace XUnitTest
         {
             Assert.Equal(1, 1);
         }
-        [Fact]
-        public void Test2()
-        {
-            Assert.Equal(1, 2);
-        }
+        
     }
 }
